0341ece9d8b65e183ec970182a0c39342588b58c
FaithDaka <53012890+FaithDaka@users.noreply.github.com>
2021-08-02 15:02:39 +0300

    mock data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Data } from './utils/data';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @Input() data= Data;
   title: string = 'Welcome to Utility Dashboard';
 }
---
+++ src/app/utils/data.ts
@@ -0,0 +1,26 @@
+export const Data =[
+    {
+        id: 1,
+        name: 'Oseon Limited',
+        number: '0773456781',
+        location: 'Kamwokya'
+    },
+    {
+        id:2,
+        name: 'Oseon Limited',
+        number: '0773456781',
+        location: 'Kamwokya'
+    },
+    {
+        id:3,
+        name: 'Oseon Limited',
+        number: '0773456781',
+        location: 'Kamwokya'
+    },
+    {
+        id:4,
+        name: 'Oseon Limited',
+        number: '0773456781',
+        location: 'Kamwokya'
+    }
+]
